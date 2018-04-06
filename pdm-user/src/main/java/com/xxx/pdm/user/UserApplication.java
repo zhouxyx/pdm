928975ce9865f126ae90443d86e486130997e7ec
zhouxyx <574931253@qq.com>
2018-04-06 21:40:35 +0800

    feign client

--- pdm-user/src/main/java/com/xxx/pdm/user/UserApplication.java
+++ pdm-user/src/main/java/com/xxx/pdm/user/UserApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  * @author zhou
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @MapperScan("com.xxx.pdm.user.mapping")
 @EnableDiscoveryClient
+@EnableFeignClients
 public class UserApplication 
 {
     public static void main( String[] args )
