e9c1c74e43b783171d49cbe97136b1ff0326434c
zhouxyx <574931253@qq.com>
2018-04-07 21:58:21 +0800

    plan

--- pdm-plan/src/main/java/com/xxx/pdm/plan/App.java
+++ pdm-plan/src/main/java/com/xxx/pdm/plan/App.java
@@ -1,11 +1,13 @@
 package com.xxx.pdm.plan;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan("com.xxx.pdm.plan.mapping")
 public class App 
 {
     public static void main( String[] args )
--- pdm-plan/src/main/java/com/xxx/pdm/plan/web/StudyPlanController.java
+++ pdm-plan/src/main/java/com/xxx/pdm/plan/web/StudyPlanController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.xxx.pdm.plan.common.ResultVo;
 import com.xxx.pdm.plan.model.StudyPlan;
 import com.xxx.pdm.plan.service.PlanService;
 
@@ -16,9 +19,21 @@ public class StudyPlanController {
 	@Autowired
 	private PlanService planService;
 
-	@GetMapping(value = "/plan/{month}")
+	@GetMapping(value = "/plans/{month}")
 	public List<StudyPlan> studyPlanByMonth(@PathVariable("month") Integer month) {
 		return planService.findStudyPlansByMonth(month);
+	}
+	
+	@PostMapping("/plans")
+	public ResultVo saveStudyPlan(@RequestBody StudyPlan studyPlan) {
+		
+		int res = planService.saveStudyPlan(studyPlan);
+		
+		if(res>0) {
+			return new ResultVo.Builder().status("200").build();
+		}else {
+			return new ResultVo.Builder().status("400").build();
+		}
 	}
 
 }
