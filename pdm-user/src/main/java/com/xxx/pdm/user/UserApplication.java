e88b7859d197cfc59040b532e3c9407f5fd2263a
zhouxyx <574931253@qq.com>
2018-04-15 22:51:43 +0800

    统一配置

--- pdm-user/src/main/java/com/xxx/pdm/user/UserApplication.java
+++ pdm-user/src/main/java/com/xxx/pdm/user/UserApplication.java
@@ -3,8 +3,6 @@ package com.xxx.pdm.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
@@ -14,12 +12,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @MapperScan("com.xxx.pdm.user.mapping")
 @EnableDiscoveryClient
 //@EnableFeignClients
-public class UserApplication extends SpringBootServletInitializer {
+public class UserApplication /*extends SpringBootServletInitializer */{
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(UserApplication.class);
-	}
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(UserApplication.class);
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
